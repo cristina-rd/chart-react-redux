a11cd91a552bd4b96112a8473824d52d8fcccd6c
Cristina Rodriguez Duque <cristina.rodriguez@agiliacenter.com>
2021-06-17 19:53:56 +0200

    added labels for the inputs

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -3,8 +3,12 @@ import React from 'react';
 export function Input(props) {
     return (
         <form onSubmit={props.addTodo}>
+            <label htmlFor='number'>Y NUMBER</label>
             <input type='number' id='number' onChange={props.addNumber}/>
+
+            <label htmlFor='text'>TEXT</label>
             <input type='text' id='text' onChange={props.addText}/>
+            
             <button>Add</button>
         </form>
     )
